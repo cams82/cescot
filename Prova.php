4136d52c852286bcb2bbcd612f441eabad7a86e9
Cams82 <camsprom@gmail.com>
2025-11-18 17:26:12 +0100

    Add launch configuration for PHP debugging and create initial Prova.php file

---
+++ Prova.php
@@ -0,0 +1,25 @@
+<?php
+
+    echo "Hello World";
+
+    echo "<br>";
+
+    $variabile = 42;
+    $result = 0;
+
+    echo "<br>";
+
+    if ($variabile > 40) {
+        $result = 1;
+
+        } else  {
+         $result = -1;
+         
+        }
+
+
+
+   
+
+
+ ?>   
